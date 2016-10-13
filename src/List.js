5370a51c498e1045c6cf68020f04320cb4ce260d
zhangzju <zhangway19921221@163.com>
2016-10-14 00:34:45 +0800

    remove idea files

--- src/List.js
+++ src/List.js
@@ -3,7 +3,10 @@ import './App.css';
 import {
   Nav,
   Container,
-  NavItem
+  NavItem,
+  Panel,
+  List,
+  ListItem
 } from 'amazeui-react';
 import {Link} from 'react-router';
 
@@ -17,7 +20,18 @@ class ButtonBar extends Component {
         <NavItem active><Link to="/list">列表</Link></NavItem>
         <NavItem><Link to="/recent">最新发布</Link></NavItem>
         </Nav>
+        <Panel>
+        信息列表
+        </Panel>
+        <List static>
+          <ListItem>脱下长日的假面</ListItem>
+          <ListItem>奔向梦幻的疆界</ListItem>
+          <ListItem>南瓜马车的午夜</ListItem>
+          <ListItem>昨天太近　明天太远</ListItem>
+          <ListItem>让我享受这感觉</ListItem>
+        </List>
       </Container>
+
       </div>
     );
   }
