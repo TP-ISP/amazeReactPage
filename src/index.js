afed635520284396ef79ee9f4ed048308665a5af
zhangzju <zhangway19921221@163.com>
2017-02-01 19:28:05 +0800

    refactor directories

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import Index from './Home';
+import Index from './components/Home';
 import {Router, Route, hashHistory} from 'react-router';
-import List from './List';
-import Recent from './Recent';
-import ErrorPage from './ErrorPage';
+import List from './components/List';
+import Recent from './components/Recent';
+import ErrorPage from './components/ErrorPage';
 
 
 
@@ -20,7 +20,7 @@ ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Index}/>
     <Route path="/list" component={List}/>
-    <Route path="/about" component={Recent}/>
+    <Route path="/recent" component={Recent}/>
   </Router>,
   document.getElementById('root')
 );
